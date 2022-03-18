2fec19fa5f0bfc9decffaab326191018276663d7
Jose Rego <jose@lightdash.com>
2022-03-18 13:47:11 +0100

    send out of office messages

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const { App } = require('@slack/bolt');
+require('dotenv').config();
+const {App} = require('@slack/bolt');
 const Analytics = require('@rudderstack/rudder-sdk-node');
-
-const identifiedUsers = {}
 
 const analyticsClient = new Analytics(
     process.env.RUDDERSTACK_WRITE_KEY,
-    `${process.env.RUDDERSTACK_DATAPLANE_URL}/v1/batch`,
+    `${process.env.RUDDERSTACK_DATA_PLANE_URL}/v1/batch`,
 );
 
 const app = new App({
@@ -13,13 +12,21 @@ const app = new App({
     signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
-app.message('', async ({ message, say }) => {
-    await say(`Hey there <@${message.user}>`);
+app.message(/.*/g, async ({message, say}) => {
+    console.log(message);
     analyticsClient.track({
         event: 'slack.message.sent',
         userId: message.user,
+        channelId: message.channel,
+        channelType: message.channel_type,
         properties: message,
     });
+    const isWeekend = new Date(message.ts).getDay() % 6 === 0;
+    const messageHours = new Date(message.ts).getUTCHours();
+    const isOutOfHours = messageHours < 8 || messageHours > 17;
+    if (isWeekend || isOutOfHours) {
+        await say(`Hey there <@${message.user}> :wave: The Lightdash team might not be available right now. We will reply as soon as we get back online`);
+    }
 });
 
 (async () => {
