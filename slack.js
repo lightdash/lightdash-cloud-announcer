14f2b912b8d158a768983ff0d89590b5c5dc4d64
oliverlaslett <11660098+owlas@users.noreply.github.com>
2025-04-10 23:12:16 +0100

    fix team id

--- slack.js
+++ slack.js
@@ -5,7 +5,11 @@ export const getTeamId = (payload) => {
     if (payload.team !== undefined) {
         return payload.team.id;
     }
+    if (payload.team_id !== undefined) {
+        return payload.team_id;
+    }
     else {
+        console.error('Payload structure for getTeamId:', JSON.stringify(payload, null, 2));
         throw new Error('Could not find a valid team id in the payload request');
     }
 }
